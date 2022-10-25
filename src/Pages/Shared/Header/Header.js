21c9991c4f736b7de05f5670bc2a886f0d057b48
Al-Amin Hossain <al-aminhossain@Al-Amins-MacBook-Air.local>
2022-10-25 21:57:13 +0600

    React Inital Project Setup

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../../Assets/Logo/logo2.svg";
+
+const Header = () => {
+  return (
+    <div>
+      <img className="img-fluid" style={{ width: "120px" }} src={logo} alt="" />
+    </div>
+  );
+};
+
+export default Header;
