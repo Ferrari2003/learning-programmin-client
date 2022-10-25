205f0a2e56afbcc474f8faa09d3fa8991b0be75d
alamin-merndev <alaminnishat@gmail.com>
2022-10-25 23:55:59 +0600

    Home Page Hero Section Complete

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,34 @@
+import Button from "react-bootstrap/Button";
 import React from "react";
+import heroBg from "../../Assets/Home/hero-bg.svg";
 
 const Home = () => {
-  return <div>This is Home</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${heroBg})`,
+        height: "90vh",
+        backgroundSize: "auto",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center right",
+      }}
+      className="hero d-flex align-items-center"
+    >
+      <div className="container w-50">
+        <div className="w-50 ">
+          <h1>Start Now Your</h1>
+          <h1>Online Education</h1>
+          <p className="text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam
+            accusamus, provident quas fuga repudiandae, ipsa nostrum eum
+            eligendi voluptas, aliquam voluptates qui perferendis laborum
+            laboriosam ut repellat necessitatibus accusantium est dolores
+          </p>
+          <Button variant="primary">Free Trial</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
