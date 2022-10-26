0f2e30fcfe40539c32bd4081096fdadee22b4c39
alamin-merndev <alaminnishat@gmail.com>
2022-10-26 11:03:46 +0600

    Home page top subject section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { FaArrowRight, FaCheck, FaRegFolderOpen } from "react-icons/fa";
+import { Col, Container, Row, Card } from "react-bootstrap";
+import { FaCheck, FaRegFolderOpen } from "react-icons/fa";
 import Hero from "./Hero/Hero";
 import "./Home.css";
 import { AiOutlineRise } from "react-icons/ai";
 import imgLern from "../../Assets/Home/learn-online-right-img.svg";
+import graphicDesign from "../../Assets/Home/15317053651529652585.svg";
+import { MdOutlineDesignServices } from "react-icons/md";
 
 const Home = () => {
   return (
@@ -57,6 +59,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+
       <Container>
         <Row>
           <Col>
@@ -108,6 +111,50 @@ const Home = () => {
               </span>
             </div>
             {/* Icon Lists End */}
+          </Col>
+        </Row>
+      </Container>
+
+      <Container>
+        <h2>Our Top Subjects</h2>
+        <Row className="text-center text-light">
+          <Col>
+            <div className="subject-box rounded shadow  bg-primary py-5">
+              <img src="https://i.ibb.co/m4LC5sh/graphic-design.png" alt="" />
+              <h4>Graphics Design</h4>
+            </div>
+          </Col>
+          <Col>
+            <div className="subject-box rounded shadow  bg-primary py-5">
+              <img src="https://i.ibb.co/fNKwVWm/web-development.png" alt="" />
+              <h4>Web Development</h4>
+            </div>
+          </Col>
+          <Col>
+            <div className="subject-box rounded shadow   bg-primary py-5">
+              <img src="https://i.ibb.co/hdNhNrC/social-media.png" alt="" />
+              <h4>Graphics Design</h4>
+            </div>
+          </Col>
+        </Row>
+        <Row className="text-center mt-4 text-light">
+          <Col>
+            <div className="subject-box rounded shadow  bg-primary py-5">
+              <img src="https://i.ibb.co/j9CJDq3/ui.png" alt="" />
+              <h4>Graphics Design</h4>
+            </div>
+          </Col>
+          <Col>
+            <div className="subject-box rounded shadow  bg-primary py-5">
+              <img src="https://i.ibb.co/Ykdw0yz/data-science.png" alt="" />
+              <h4>Web Development</h4>
+            </div>
+          </Col>
+          <Col>
+            <div className="subject-box rounded shadow   bg-primary py-5">
+              <img src="https://i.ibb.co/RPxWrH0/deep-learning.png" alt="" />
+              <h4>Graphics Design</h4>
+            </div>
           </Col>
         </Row>
       </Container>
